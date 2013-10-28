8e7d555be1ff69c805ac2f926cef4162f757576f
Scott Waller <walles2@rpi.edu>
2013-10-28 17:27:50 -0400

    start working on mainwidget

--- Tabloid/mainwidget.cpp
+++ Tabloid/mainwidget.cpp
@@ -1,1 +1,12 @@
 
+
+
+#include <QLabel>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QStyle>
+#include <QMenu>
+#include <QFileDialog>
+#include <QTimerEvent>
+#include <QMessageBox>
