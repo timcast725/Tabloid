da8449040626fa6221e9cd938c8674f3f2831fc4
Gary Lu <luyijia@gmail.com>
2013-10-30 17:07:27 -0400

    Improved test with beats and better accuracy

--- test/test.cpp
+++ test/test.cpp
@@ -1,20 +1,13 @@
 #include <fstream>
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
+#include <map>
 #include <math.h>
-#include <stdio.h>
+#include <sstream>
 #include <stdlib.h>
-#include <aubio/aubio.h>
-// #include <python2.7/Python.h>
+#include <string>
 
-std::string frequencyToNote(float frequency)
+std::string midiToNote(int pitch)
 {
-    if (frequency < 0)
-        return "none";
-
-    int pitch = 57.50 + 12 * log2(frequency / 440.0);
     int octave = pitch / 12;
     std::string note;
     switch (pitch % 12)
@@ -64,6 +57,15 @@ std::string frequencyToNote(float frequency)
     return note;
 }
 
+std::string frequencyToNote(double frequency)
+{
+    if (frequency < 0)
+        return "none";
+
+    int pitch = 57.50 + 12 * log2(frequency / 440.0);
+    return midiToNote(pitch);
+}
+
 int main(int argc, char const *argv[])
 {
     if (argc != 2)
@@ -71,29 +73,62 @@ int main(int argc, char const *argv[])
         std::cerr << "yo put in a file name yo\n";
         return 1;
     }
-    // Hard coding shiz
-    std::string command = "python aubiopitch.py -i ";
     std::string filename = argv[1];
-    command += filename;
-    command += " > test.txt";
-    // Py_Initialize();
-    system(command.c_str());
 
-    std::ifstream frequency_text;
-    frequency_text.open("test.txt");
-    float old_tick = 0;
-    float tick;
-    float frequency;
-    std::string note;
-    while (frequency_text >> tick >> frequency)
+    // Hard coding shiz
+    std::string pitch = "python aubiopitch.py -i ";
+    std::string target = " > test.txt";
+    pitch += filename;
+    pitch += target;
+    system(pitch.c_str());
+
+    std::ifstream output;
+    output.open("test.txt");
+    double tick;
+    double frequency;
+    std::map<double, double> notes;
+    while (output >> tick >> frequency)
     {
-        if (frequency > 0 && note != frequencyToNote(frequency))
+        if (frequency > 0)
         {
-            note = frequencyToNote(frequency);
-            std::cout << tick - old_tick << " " << note << " (" << frequency << ")" << std::endl;
-            old_tick = tick;
+            // note = frequencyToNote(frequency);
+            // std::cout << tick << " " << note << " (" << frequency << ")" << std::endl;
+            notes[tick] = frequency;
         }
     }
+    output.close();
+
+    std::string onset = "python aubiocut.py -i ";
+    onset += filename;
+    onset += target;
+    system(onset.c_str());
+    output.open("test.txt");
+    std::string note;
+    std::map<double, double>::iterator itr = notes.begin();
+    double old_tick = 0.0;
+    double average;
+    double count;
+    while (output >> tick)
+    {
+        average = 0.0;
+        count = 0.0;
+        while (itr->first < tick)
+        {
+            average += itr->second;
+            itr++;
+            count += 1.0;
+        }
+        if (count > 0.0)
+        {
+            average /= count;
+            note = frequencyToNote(average);
+            std::cout << note << " at " << old_tick << " seconds " << std::endl;
+        }
+        old_tick = tick;
+    }
+
+    // SF_INFO info;
+    // SNDFILE *wav = sf_open(argv[1], SFM_READ, &info);
 
     return 0;
 }
